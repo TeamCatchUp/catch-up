67f0d85c1f3c3f36a063a87f92a8a86f8f65cc87
jy000n <jayoonchang@naver.com>
2026-01-02 08:40:48 +0900

    feat: TopNavbar UX, SEO 코드 개선 #3

--- src/components/common/topNavbar/TopNavbar.tsx
+++ src/components/common/topNavbar/TopNavbar.tsx
@@ -22,7 +22,7 @@ const TopNavbar = () => {
   };
 
   return (
-    <nav className="border-neutral-3 h-full w-full border-b">
+    <nav aria-label="메인 네비게이션" className="border-neutral-3 h-full w-full border-b">
       <div className="flex justify-between px-10 py-2">
         <div className="flex items-center justify-center">
           <Link href="/">
@@ -32,27 +32,38 @@ const TopNavbar = () => {
             </button>
           </Link>
         </div>
-        <div className="flex items-center justify-center gap-2">
-          <button
-            onClick={toggleShareModal}
-            className={`text-body-small cursor-pointer rounded-lg border px-2.5 py-1.5 transition-colors ${
-              isShareModalOpen
-                ? 'border-neutral-4 bg-neutral-2 active:border-neutral-5 active:bg-neutral-3'
-                : 'border-neutral-3 active:border-neutral-5 active:bg-neutral-3 hover:border-neutral-4 hover:bg-neutral-2 bg-white'
-            } `}
-          >
-            공유
-          </button>
-          <button
-            className={`${
-              isMoreModalOpen
-                ? 'border-neutral-4 bg-neutral-2 active:border-neutral-5 active:bg-neutral-3'
-                : 'border-neutral-3 active:border-neutral-5 active:bg-neutral-3 hover:border-neutral-4 hover:bg-neutral-2 bg-white'
-            } cursor-pointer rounded-lg border px-1.5 py-1.5 transition-colors`}
-          >
-            <Kebeb_2 onClick={toggleMoreModal} className="h-6 w-6" />
-          </button>
-        </div>
+
+        <ul className="flex items-center justify-center gap-2">
+          <li>
+            <button
+              onClick={toggleShareModal}
+              aria-expanded={isShareModalOpen}
+              aria-controls="share-modal"
+              className={`text-body-small cursor-pointer rounded-lg border px-2.5 py-1.5 transition-colors ${
+                isShareModalOpen
+                  ? 'border-neutral-4 bg-neutral-2 active:border-neutral-5 active:bg-neutral-3'
+                  : 'border-neutral-3 active:border-neutral-5 active:bg-neutral-3 hover:border-neutral-4 hover:bg-neutral-2 bg-white'
+              } `}
+            >
+              공유
+            </button>
+          </li>
+
+          <li>
+            <button
+              aria-expanded={isMoreModalOpen}
+              aria-controls="more-modal"
+              onClick={toggleMoreModal}
+              className={`${
+                isMoreModalOpen
+                  ? 'border-neutral-4 bg-neutral-2 active:border-neutral-5 active:bg-neutral-3'
+                  : 'border-neutral-3 active:border-neutral-5 active:bg-neutral-3 hover:border-neutral-4 hover:bg-neutral-2 bg-white'
+              } cursor-pointer rounded-lg border px-1.5 py-1.5 transition-colors`}
+            >
+              <Kebeb_2 className="h-6 w-6" />
+            </button>
+          </li>
+        </ul>
       </div>
 
       {/* 공유 모달 */}
