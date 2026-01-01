33ff00e42afe9c2c860c9621057b5b2ec0e6c571
jy000n <jayoonchang@naver.com>
2026-01-01 20:36:08 +0900

    Revert "Merge pull request #1 from TeamCatchUp/feature/layout"
    This reverts commit 0bb853cf47eb817224d1402bb79796341ee0a63a, reversing
    changes made to 50d5ce8b4d12931fc460dab24b16800bb4684356.

--- src/components/common/navbar/ClosedSideNavbar.tsx
+++
@@ -1,5 +0,0 @@
-const ClosedSideNavbar = () => {
-  return <nav className="border-neutral-3 h-full w-full border-r"></nav>;
-};
-
-export default ClosedSideNavbar;
--- src/components/common/navbar/OpenedSideNavbar.tsx
+++
@@ -1,5 +0,0 @@
-const OpenedSideNavbar = () => {
-  return <nav className="border-neutral-3 h-full w-full border-r">opened</nav>;
-};
-
-export default OpenedSideNavbar;
