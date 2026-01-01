8d3dc44d8d2281d04acdd05ef987f06f1f1fc9c1
jy000n <jayoonchang@naver.com>
2026-01-01 23:33:19 +0900

    feat: 홈페이지 TopNavbar UI #3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { Metadata } from 'next';
 import '@/styles/globals.css';
 import OpenedSideNavbar from '@/components/common/navbar/OpenedSideNavbar';
 // import ClosedSideNavbar from '@/components/common/navbar/ClosedSideNavbar';
-import TopNabar from '@/components/common/navbar/TopNavbar';
+import TopNavbar from '@/components/common/navbar/TopNavbar';
 
 export const metadata: Metadata = {
   title: 'CATCHUP',
@@ -12,14 +12,14 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="ko">
-      <body className="h-screen overflow-hidden">
+      <body className="h-screen">
         <div className="flex h-full">
           <aside className="w-60 flex-shrink-0">
             <OpenedSideNavbar />
           </aside>
           <div className="flex flex-1 flex-col">
             <header className="h-13">
-              <TopNabar />
+              <TopNavbar />
             </header>
             <main className="flex-1 overflow-y-auto">{children}</main>
           </div>
--- src/components/common/navbar/TopNavbar.tsx
+++ src/components/common/navbar/TopNavbar.tsx
@@ -1,5 +1,27 @@
+import Home from '@/assets/svgs/navbar/home.svg';
+import Kebeb_2 from '@/assets/svgs/navbar/kebeb_2.svg';
+
 const TopNavbar = () => {
-  return <nav className="border-neutral-3 h-full w-full border-b">top</nav>;
+  return (
+    <nav className="border-neutral-3 h-full w-full border-b">
+      <div className="flex justify-between px-10 py-2">
+        <div className="flex items-center justify-center">
+          <button className="flex cursor-pointer gap-2">
+            <Home className="h-6 w-6" />
+            <span className="text-heading-medium">홈</span>
+          </button>
+        </div>
+        <div className="flex items-center justify-center gap-2">
+          <button className="text-body-small active:border-neutral-5 active:bg-neutral-3 hover:border-neutral-4 hover:bg-neutral-2 border-neutral-3 cursor-pointer rounded-lg border px-2.5 py-1.5 transition-colors">
+            공유
+          </button>
+          <button className="border-neutral-3 hover:border-neutral-4 active:border-neutral-5 active:bg-neutral-3 hover:bg-neutral-2 cursor-pointer rounded-lg border px-1.5 py-1.5 transition-colors">
+            <Kebeb_2 className="h-6 w-6" />
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default TopNavbar;
