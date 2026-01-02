4c06e9ef7b1e34d233d88090b47278232bcb1390
jy000n <jayoonchang@naver.com>
2026-01-02 22:43:08 +0900

    fix: enable next static export for cloudlfare pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: 'export',
+
+  images: {
+    unoptimized: true,
+  },
+
   turbopack: {},
   webpack(config) {
     config.module.rules.push({
