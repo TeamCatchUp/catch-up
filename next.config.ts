684a25a4348d96eb6781ea9351a09a0bf470115f
jy000n <jayoonchang@naver.com>
2026-01-02 18:23:06 +0900

    fix: turbopack-webpack 배포 에러 해결 중

--- next.config.ts
+++ next.config.ts
@@ -1,37 +1,34 @@
 import type { NextConfig } from 'next';
+import path from 'path';
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   reactStrictMode: true,
-  turbopack: {},
 
-  webpack: (config) => {
-    // @ts-expect-error 타입 에러 무시
-    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
+  experimental: {
+    // @ts-expect-error Next 16 turbo option
+    turbo: false,
+  },
 
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/,
-      },
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
-        use: [
-          {
-            loader: '@svgr/webpack',
-            options: {
-              typescript: true,
-              ext: 'tsx',
-            },
-          },
-        ],
-      },
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find(
+      (rule: any) => rule?.test instanceof RegExp && rule.test.test('.svg'),
     );
+
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: fileLoaderRule.issuer,
+      use: ['@svgr/webpack'],
+    });
+
     fileLoaderRule.exclude = /\.svg$/i;
+
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@': path.resolve(__dirname, 'src'),
+    };
+
     return config;
   },
-};
+} satisfies NextConfig;
 
 export default nextConfig;
