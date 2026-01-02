1cb2d752eccad5089ef6cb80dfc9ea748f245a0a
jy000n <jayoonchang@naver.com>
2026-01-03 04:33:00 +0900

    feat: 홈페이지 'Catch Up을 활용하는 방법' 카드 UI #10

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import HomeTopNavbar from '@/components/home/topNavbar/TopNavbar';
+import HowToUse from '@/components/home/cardComponents/HowToUse';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen">
+    <div className="flex flex-col">
       <HomeTopNavbar />
+      {/* 검색 */}
+      <div className="flex flex-col items-center gap-16 px-12 py-10">
+        <HowToUse />
+      </div>
     </div>
   );
 }
