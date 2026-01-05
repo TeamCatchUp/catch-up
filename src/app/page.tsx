8380ea6015191493239a2d4cf25bbbe0043f7b0d
yam <1669jsh@naver.com>
2026-01-05 21:31:38 +0900

    feat: 메인페이지에 검색창 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,13 @@ import HomeTopNavbar from '@/components/home/topNavbar/TopNavbar';
 import HowToUse from '@/components/home/cardComponents/HowToUse';
 import LinkTool from '@/components/home/cardComponents/LinkTool';
 import RecentlyChecked from '@/components/home/cardComponents/RecentlyChecked';
+import Search from './search/page';
 
 export default function Home() {
   return (
     <div className="bg-home-gradient flex flex-col">
       <HomeTopNavbar />
-      {/* 검색 */}
+      <Search />
       <div className="flex flex-col items-center gap-16 px-12 py-10">
         <RecentlyChecked />
         <HowToUse />
