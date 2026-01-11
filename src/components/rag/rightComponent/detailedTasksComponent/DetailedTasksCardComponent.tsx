29d723cc075af2665ac2343a17dd625e411c9162
jy000n <jayoonchang@naver.com>
2026-01-12 08:34:10 +0900

    feat: RAG 답변 페이지 상세 업무 컴포넌트 스켈레톤 UI #29

---
+++ src/components/rag/rightComponent/detailedTasksComponent/DetailedTasksCardComponent.tsx
@@ -0,0 +1,10 @@
+const DetailedTasksCardComponent = () => {
+  return (
+    <div className="flex flex-col">
+      <div className="">카드카드</div>
+      <div className="">카드카드</div>
+    </div>
+  );
+};
+
+export default DetailedTasksCardComponent;
--- src/components/rag/rightComponent/detailedTasksComponent/DetailedTasksComponent.tsx
+++ src/components/rag/rightComponent/detailedTasksComponent/DetailedTasksComponent.tsx
@@ -1,7 +1,27 @@
+import clsx from 'clsx';
+import { useState } from 'react';
+import Align from '/public/icons/icon/align.svg';
+import Divider from '/public/icons/icon/divider.svg';
+import RagDetailedTasksSkeleton from '@/components/Skeleton/RagDetailedTasksSkeleton';
+import DetailedTasksCardComponent from './DetailedTasksCardComponent';
+
 const DetailedTasksComponent = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <div className="">
-      <div className="">상세 업무 컴포넌트</div>
+    <div className="flex w-101.25 flex-col gap-3 px-4 py-3">
+      <div className="-mb-4 flex w-full overflow-x-auto">
+        <div className="flex h-9 min-w-max items-center gap-0.5">
+          <button className="icon-button-only-gray flex h-8 w-8 shrink-0 cursor-pointer items-center justify-center rounded-lg p-0.5">
+            <Align className="block h-6 w-6 text-gray-50" />
+          </button>
+          <Divider className="text-neutral-4 block h-6 w-6 shrink-0" />
+        </div>
+      </div>
+
+      <div className="mt-1 flex flex-col gap-2">
+        {isLoading ? <RagDetailedTasksSkeleton /> : <DetailedTasksCardComponent />}
+      </div>
     </div>
   );
 };
