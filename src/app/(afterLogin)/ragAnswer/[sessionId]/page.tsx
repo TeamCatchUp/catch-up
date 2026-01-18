5a691a3cdc1b22f86be4d80a5d1537f874036b76
jy000n <jayoonchang@naver.com>
2026-01-18 22:23:32 +0900

    RAG 답변 페이지 스페이스명 + 필터링 부분 수정

--- src/app/(afterLogin)/ragAnswer/[sessionId]/page.tsx
+++ src/app/(afterLogin)/ragAnswer/[sessionId]/page.tsx
@@ -380,31 +380,33 @@ export default function Page() {
                             </div>
                             <DropDown className="text-gray-70 relative bottom-px h-4 w-4 shrink-0" />
                           </div> */}
-                          <div className="group relative flex gap-1">
-                            <div
-                              onClick={(e) => {
-                                e.stopPropagation();
-                                setIsSpaceDropDownOpen((prev) => !prev);
-                              }}
-                              className="icon-button-only-gray flex cursor-pointer items-center gap-1 px-2 py-1"
-                            >
-                              <div className="text-body-small text-gray-70 relative top-px block w-32 truncate px-2 py-1">
-                                스페이스명 text text text
+                          <div className="relative flex gap-1">
+                            <div className="group relative w-fit gap-1">
+                              <div
+                                onClick={(e) => {
+                                  e.stopPropagation();
+                                  setIsSpaceDropDownOpen((prev) => !prev);
+                                }}
+                                className="icon-button-only-gray flex cursor-pointer items-center gap-1 px-2 py-1"
+                              >
+                                <div className="text-body-small text-gray-70 relative top-px block w-32 truncate px-2 py-1">
+                                  스페이스명 text text text
+                                </div>
+                                <DropDown
+                                  className={clsx(
+                                    'text-gray-70 relative bottom-px h-4 w-4 shrink-0',
+                                    isSpaceDropDownOpen ? 'rotate-180' : '',
+                                  )}
+                                />
                               </div>
-                              <DropDown
-                                className={clsx(
-                                  'text-gray-70 relative bottom-px h-4 w-4 shrink-0',
-                                  isSpaceDropDownOpen ? 'rotate-180' : '',
-                                )}
-                              />
-                            </div>
-                            <div className="absolute bottom-12.5 left-23.75">
-                              <ToolTip text={'답변 기준 팀스페이스 변경하기'} />
+                              <div className="absolute bottom-12.5 left-23.75">
+                                <ToolTip text={'답변 기준 팀스페이스 변경하기'} />
+                              </div>
                             </div>
                           </div>
                           {/* TeamSpace 드롭다운 모달 */}
                           {isSpaceDropDownOpen && (
-                            <div className="absolute top-20 z-100">
+                            <div className="absolute top-32 z-100">
                               <TeamSpaceModal
                                 onClose={() => {
                                   setIsSpaceDropDownOpen(false);
