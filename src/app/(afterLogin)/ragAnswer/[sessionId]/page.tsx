06197ca25346e1e176a582ad0192fe70759ac2d3
jy000n <jayoonchang@naver.com>
2026-01-23 10:21:44 +0900

    fix: button.css 적용 문제 해결

--- src/app/(afterLogin)/ragAnswer/[sessionId]/page.tsx
+++ src/app/(afterLogin)/ragAnswer/[sessionId]/page.tsx
@@ -550,10 +550,6 @@ export default function Page() {
                     ) : (
                       <div className="group relative max-w-full">
                         <span className="text-heading-xlarge text-gray-70 mr-5">{msg.content}</span>
-                        {/* <button
-                          onClick={() => setEditingMessageId(msg.id)}
-                          className="border-neutral-3 box-button-outline-gray inline-flex translate-y-1 cursor-pointer justify-center gap-1 rounded-lg border px-2 py-1 opacity-0 transition-opacity group-hover:opacity-100"
-                        > */}
                         <button
                           onClick={() => setEditingMessageId(msg.id)}
                           className={clsx(
