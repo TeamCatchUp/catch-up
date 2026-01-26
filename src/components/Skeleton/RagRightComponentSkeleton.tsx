14d3c2edb6c7e79efcbb7e3fa2dcbb4de83ecd97
jy000n <jayoonchang@naver.com>
2026-01-27 08:08:39 +0900

    style: RAG 답변 페이지 우측 source 스타일 조정

--- src/components/Skeleton/RagRightComponentSkeleton.tsx
+++ src/components/Skeleton/RagRightComponentSkeleton.tsx
@@ -6,17 +6,17 @@ interface RagDetailedTasksSkeletonProps {
 
 const RagRightComponentSkeleton = ({ message }: RagDetailedTasksSkeletonProps) => {
   return (
-    <div className="mt-3 flex w-115 flex-col items-center justify-center gap-5">
+    <div className="relative left-1 mt-3 flex w-105 flex-col items-center justify-center gap-5 px-2">
       <div className="bg-blue-1 flex w-105 animate-pulse flex-col items-center justify-center gap-5 rounded-2xl p-5">
         <SourceFile className="h-23 w-28.75" />
         <span className="text-body-small text-gray-30 whitespace-nowrap">{message}</span>
       </div>
       <div className="flex animate-pulse flex-col gap-5">
-        <div className="bg-neutral-1 h-7.5 w-107 rounded-lg"></div>
-        <div className="bg-neutral-1 h-7.5 w-85.75 rounded-lg"></div>
-        <div className="bg-neutral-1 h-7.5 w-74.5 rounded-lg"></div>
-        <div className="bg-neutral-1 h-7.5 w-60.75 rounded-lg"></div>
-        <div className="bg-neutral-1 h-7.5 w-44.5 rounded-lg"></div>
+        <div className="bg-neutral-1 h-7.5 w-105 rounded-lg" />
+        <div className="bg-neutral-1 h-7.5 w-85.75 rounded-lg" />
+        <div className="bg-neutral-1 h-7.5 w-74.5 rounded-lg" />
+        <div className="bg-neutral-1 h-7.5 w-60.75 rounded-lg" />
+        <div className="bg-neutral-1 h-7.5 w-44.5 rounded-lg" />
       </div>
     </div>
   );
--- src/components/rag/rightComponent/sourceComponent/SourceComponent.tsx
+++ src/components/rag/rightComponent/sourceComponent/SourceComponent.tsx
@@ -113,7 +113,7 @@ const SourceComponent = ({ sources, isLoading, isError }: Props) => {
             {/* divider */}
             {recommendedSources.length > 0 && (
               <>
-                <div className="bg-neutral-4 mt-1 mb-4 h-px w-93.25" />
+                <div className="bg-neutral-4 mt-1 mb-4 h-px w-107" />
 
                 <div className="flex flex-col gap-2.5">
                   <div className="flex items-center gap-1.5 px-1.5">
