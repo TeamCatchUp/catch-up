3f2358850402667c9da3987e59851e72c3415c9e
jy000n <jayoonchang@naver.com>
2026-01-29 07:04:40 +0900

    fix: RAG 답변 페이지 헤더 CatchAssistantModal 타입 에러로 인한 배포 실패 해결

--- src/components/rag/modal/CatchAssistantModal.tsx
+++ src/components/rag/modal/CatchAssistantModal.tsx
@@ -103,7 +103,7 @@ const CatchAssistantModal = ({ onClose }: CatchAssistantModalProps) => {
       {isLoading ? (
         <div className="text-gray-40 p-5">데이터를 불러오는 중입니다...</div>
       ) : (
-        <SearchHistory querys={recentQueries} isModal="true" />
+        <SearchHistory querys={recentQueries} isModal={true} />
       )}
     </div>
   );
