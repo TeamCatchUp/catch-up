74f40b41b1b3eeddbce6179ec2b4aa06d44c75cc
jy000n <jayoonchang@naver.com>
2026-01-29 12:33:21 +0900

    style: RAG 답변 페이지 답변-출처 연계 스타일 수정 #64

--- src/components/rag/answerComponent/markdown/SourceBadge.tsx
+++ src/components/rag/answerComponent/markdown/SourceBadge.tsx
@@ -6,7 +6,7 @@ const SourceBadge = ({ n, sourceType }: { n: string; sourceType: SourceType }) =
   return (
     <span
       className={clsx(
-        'mx-px h-6 w-6.5 items-center justify-center rounded-full px-2 py-1 whitespace-nowrap',
+        'mr-px h-6 w-6.5 items-center justify-center rounded-full px-2 py-1 whitespace-nowrap',
         sourceType === 'jira' ? 'bg-green-10' : 'bg-blue-5',
       )}
     >
