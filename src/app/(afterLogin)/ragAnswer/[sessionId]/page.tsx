bda66c569dbad6d799b018b596f65052c38c720d
jy000n <jayoonchang@naver.com>
2026-01-29 17:26:16 +0900

    feat: RAG 답변 페이지 page indicator 다시 렌더링

--- src/app/(afterLogin)/ragAnswer/[sessionId]/page.tsx
+++ src/app/(afterLogin)/ragAnswer/[sessionId]/page.tsx
@@ -994,8 +994,8 @@ export default function Page() {
             </div>
 
             {/* 페이지 인디케이터 */}
-            {/* {qaPairs.length > 1 && (
-              <div className="mt-4 flex items-center gap-2">
+            {qaPairs.length > 1 && (
+              <div className="mt-7 flex items-center gap-2">
                 {qaPairs.map((_, idx) => (
                   <button
                     key={idx}
@@ -1027,7 +1027,7 @@ export default function Page() {
                   />
                 ))}
               </div>
-            )} */}
+            )}
           </div>
 
           {/* 입력창 */}
