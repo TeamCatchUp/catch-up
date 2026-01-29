ebc2cdaa55e81fa87ce97a087c610448f7ab6f92
jy000n <jayoonchang@naver.com>
2026-01-29 12:39:12 +0900

    style: RAG 답변 페이지 답변-출처 연계 스타일 태그 span->div 변경 #64

--- src/components/rag/answerComponent/markdown/SourceBadge.tsx
+++ src/components/rag/answerComponent/markdown/SourceBadge.tsx
@@ -4,14 +4,14 @@ export type SourceType = 'jira' | 'github';
 
 const SourceBadge = ({ n, sourceType }: { n: string; sourceType: SourceType }) => {
   return (
-    <span
+    <div
       className={clsx(
         'mr-px h-6 w-6.5 items-center justify-center rounded-full px-2 py-1 whitespace-nowrap',
         sourceType === 'jira' ? 'bg-green-10' : 'bg-blue-5',
       )}
     >
       <span className="text-body-xsmall text-gray-80 relative bottom-px">{n}</span>
-    </span>
+    </div>
   );
 };
 
