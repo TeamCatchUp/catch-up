973f86e6676323b55bd27fdc875a368f04bf03c5
yam <1669jsh@naver.com>
2026-01-29 17:59:03 +0900

    feat:캐치스탄트 AI 사용법 카드 기능 구현

--- src/app/(afterLogin)/search/layout.tsx
+++ src/app/(afterLogin)/search/layout.tsx
@@ -1,15 +1,24 @@
+'use client'; 
+
+import { useState } from 'react';
 import TopNavbar from '@/components/common/topNavbar/TopNavbar';
-import IconCancel from '@/public/icons/icon/cancel.svg';
 import IconLightbulb from '@/public/icons/icon/lightbulb.svg';
 import Jira from '@/public/image/catchstantJira1.jpg';
 import Git from '@/public/image/catchstantGit1.jpg';
 import Image from 'next/image';
+import { GithubGuideCard, JiraGuideCard } from '@/components/UI/AIGuideUI';
 
 export default function SearchLayout({ children }: { children: React.ReactNode }) {
+  const [activeCard, setActiveCard] = useState<'jira' | 'git' | null>(null);
+
+  const toggleCard = (type: 'jira' | 'git') => {
+    setActiveCard(activeCard === type ? null : type);
+  };
+
   return (
     <div className="bg-home-gradient flex flex-[1_0_0] flex-col items-start self-stretch">
       <TopNavbar pageType="home" />
-      <div className="felx h-screen flex-col items-start self-stretch">
+      <div className="felx min-h-screen flex-col items-start self-stretch">
         {children}
         <div className="flex flex-col items-center gap-4 self-stretch px-52 pt-10 pb-30">
           <div className="flex w-190 flex-col items-center gap-3">
@@ -18,15 +27,20 @@ export default function SearchLayout({ children }: { children: React.ReactNode }
                 <IconLightbulb className="text-blue-30" />
                 <div className="text-heading-small text-gray-50">캐치스턴트 AI에서 정확한 답변을 얻으려면</div>
               </div>
-              <div className="flex h-7 w-7 items-center justify-center gap-2.5 p-0.5">
-                <IconCancel />
-              </div>
             </div>
+
             <div className="flex items-start gap-6 self-stretch">
-              <Image src={Jira} alt="jira" className="w-[368px]" />
-              <Image src={Git} alt="git" className="w-[368px]" />
+              <button onClick={() => toggleCard('jira')} className={`transition-transform hover:scale-[1.02]`}>
+                <Image src={Jira} alt="jira" className="w-[368px] cursor-pointer" />
+              </button>
+
+              <button onClick={() => toggleCard('git')} className={`transition-transform hover:scale-[1.02]`}>
+                <Image src={Git} alt="git" className="w-[368px] cursor-pointer" />
+              </button>
             </div>
           </div>
+          {activeCard === 'jira' && <JiraGuideCard onClose={() => setActiveCard(null)} />}
+          {activeCard === 'git' && <GithubGuideCard onClose={() => setActiveCard(null)} />}
         </div>
       </div>
     </div>
