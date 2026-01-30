004262134ca0f75bab313bb5b0437d666f2b3b22
yam <1669jsh@naver.com>
2026-01-30 14:03:11 +0900

    fix: 오타수정

--- src/components/common/sideNavBar/SideNavBar.tsx
+++ src/components/common/sideNavBar/SideNavBar.tsx
@@ -106,7 +106,7 @@ const SideNavBar = () => {
     const fetchDefaultData = async () => {
       try {
         setIsLoading(true);
-        const [chatroomRes] = await Promise.all([searchService.getRecentChatromms()]);
+        const [chatroomRes] = await Promise.all([searchService.getRecentChatrooms()]);
 
         if (chatroomRes.content) {
           const mappedChatrooms = chatroomRes.content.map((item: any) => ({
