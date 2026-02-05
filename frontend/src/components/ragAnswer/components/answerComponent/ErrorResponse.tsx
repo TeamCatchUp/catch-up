0281952dfc80e79fff9a1b419d43e2ee430608c5
Wannys26 <119491157+Wannys26@users.noreply.github.com>
2026-02-05 16:33:01 +0900

    refactor(frontend): rag 컴포넌트를 ragAnswer/components로 이동

--- frontend/src/components/ragAnswer/components/answerComponent/ErrorResponse.tsx
+++ frontend/src/components/ragAnswer/components/answerComponent/ErrorResponse.tsx
@@ -1,6 +1,6 @@
 import Error from '/public/icons/icon/error.svg';
-import AnswerActionButtons from '@/components/rag/answerComponent/AnswerActionButtons';
-import FeedbackSection from '@/components/rag/answerComponent/FeedbackSection';
+import AnswerActionButtons from '@/components/ragAnswer/components/answerComponent/AnswerActionButtons';
+import FeedbackSection from '@/components/ragAnswer/components/answerComponent/FeedbackSection';
 
 const ErrorResponse = ({
   icons,
