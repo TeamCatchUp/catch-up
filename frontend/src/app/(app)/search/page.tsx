025ab383356d8888702106e837e38dfe641494e0
fkgrkyr <fkgrkyr@gmail.com>
2026-02-09 16:30:32 +0900

    fix(frontend): search 페이지 SVG 아이콘 크기 className 추가

--- frontend/src/app/(app)/search/page.tsx
+++ frontend/src/app/(app)/search/page.tsx
@@ -80,7 +80,7 @@ export default function Search() {
           <div className="flex w-190 flex-col items-center gap-3">
             <div className="flex items-center justify-between self-stretch">
               <div className="flex items-center gap-2">
-                <IconLightbulb className="text-blue-30" />
+                <IconLightbulb className="size-4.5 text-blue-30" />
                 <div className="text-heading-small text-gray-50">캐치스턴트 AI에서 정확한 답변을 얻으려면</div>
               </div>
             </div>
