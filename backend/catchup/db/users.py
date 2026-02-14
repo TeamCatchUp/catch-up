35db198ff8118409d029976dc91350bb276229f2
ba2slk <0730bss@gmail.com>
2026-02-14 21:55:08 +0900

    refactor(backend): execute -> scalar 변경

--- backend/catchup/db/users.py
+++ backend/catchup/db/users.py
@@ -9,7 +9,10 @@ def get_user_by_email(
     db: Session,
     email: str
 ) -> User | None:
-    return db.execute(select(User).filter_by(email=email)).scalar_one_or_none()
+    return db.scalar(
+        select(User)
+        .filter_by(email=email)
+    )
 
 
 def create_new_user(
@@ -17,9 +20,7 @@ def create_new_user(
     user_create: UserCreate
 ) -> User:
     new_user = User(**user_create.model_dump())
-
     db.add(new_user)
-    
     return new_user
 
 
@@ -29,7 +30,6 @@ def update_user_refresh_token(
     refresh_token: str | None
 ):
     stmt = update(User).where(User.id == user_id).values(refresh_token=refresh_token)
-    
     db.execute(stmt)
 
 
@@ -37,7 +37,6 @@ def get_user_with_full_context(
     db: Session,
     user_id: int
 ) -> User | None:
-
     stmt = (
         select(User)
         .where(User.id == user_id)
@@ -47,5 +46,4 @@ def get_user_with_full_context(
             .selectinload(Workspace.company)
         )
     )
-    
-    return db.scalars(stmt).one_or_none()
+    return db.scalar(stmt)
