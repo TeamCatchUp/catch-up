601bc15e0da3f460f273e9efa141939d6ff59d81
hxxukii <81512847+gilbert09031@users.noreply.github.com>
2026-02-21 19:56:31 +0900

    feat(backend): 마이페이지 엔드포인트 개설

--- backend/catchup/server/auth/schemas.py
+++ backend/catchup/server/auth/schemas.py
@@ -9,6 +9,15 @@ class CurrentUserInfo(BaseModel):
     role: str
     status: UserStatus
 
+class CurrentUserProfile(BaseModel):
+    name: str
+    email: EmailStr
+    picture: str
+    department: str
+    job_level: str
+
+    
+
 
 class TokenRefreshResponse(BaseModel):
     status: str
