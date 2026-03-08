2f9eb580c56508954eaeed0ffcf9c8b6c486894a
fkgrkyr <fkgrkyr@gmail.com>
2026-03-08 21:04:12 +0900

    refactor(frontend): Calendar 컴포넌트 hover:bg-blue-50 프리미티브를 시맨틱 토큰으로 전환

--- frontend/src/shared/components/ui/calendar.tsx
+++ frontend/src/shared/components/ui/calendar.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import * as React from 'react';
 import { DayPicker, type DayPickerProps } from 'react-day-picker';
 
 import IconArrowLeft from '@/public/icons/icon/arrow_left.svg';
@@ -30,9 +29,9 @@ function Calendar({ className, classNames, ...props }: DayPickerProps) {
         today: '',
         selected: '',
         range_start:
-          "rdp-range_start [&>button]:bg-fill-primary [&>button]:text-white [&>button]:hover:bg-fill-primary before:absolute before:inset-y-0 before:right-0 before:w-1/2 before:bg-fill-interaction-pressed before:content-['']",
+          "rdp-range_start [&>button]:bg-fill-primary [&>button]:text-white [&>button]:hover:bg-fill-primary-interaction-hover before:absolute before:inset-y-0 before:right-0 before:w-1/2 before:bg-fill-interaction-pressed before:content-['']",
         range_end:
-          "rdp-range_end [&>button]:bg-fill-primary [&>button]:text-white [&>button]:hover:bg-fill-primary before:absolute before:inset-y-0 before:left-0 before:w-1/2 before:bg-fill-interaction-pressed before:content-['']",
+          "rdp-range_end [&>button]:bg-fill-primary [&>button]:text-white [&>button]:hover:bg-fill-primary-interaction-hover before:absolute before:inset-y-0 before:left-0 before:w-1/2 before:bg-fill-interaction-pressed before:content-['']",
         range_middle: 'bg-fill-interaction-pressed',
         outside: 'text-content-assistive',
         disabled: 'text-content-assistive opacity-50',
