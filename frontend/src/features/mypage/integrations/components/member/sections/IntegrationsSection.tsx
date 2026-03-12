846eb1046b70361f2fb88310a6830a65693a11e7
fkgrkyr <fkgrkyr@gmail.com>
2026-03-13 03:18:12 +0900

    feat(frontend): 임베딩 진행 현황 패널 구현

--- frontend/src/features/mypage/integrations/components/member/sections/IntegrationsSection.tsx
+++ frontend/src/features/mypage/integrations/components/member/sections/IntegrationsSection.tsx
@@ -2,9 +2,11 @@
 
 import { useCallback, useMemo, useState } from 'react';
 
+import { MOCK_CONNECTOR_PROGRESS } from '../../../constants/mockSyncData';
 import { useMemberIntegrationViewModel } from '../../../hooks/useMemberIntegrationViewModel';
 import type { SyncFilterType } from '../../../types/api';
 import { buildMemberDisplayRows } from '../../../utils/memberDisplay';
+import EmbeddingProgressPanel from '../panels/EmbeddingProgressPanel';
 import StatusCardsSection from './StatusCardsSection';
 import UsersStatusSection from './UsersStatusSection';
 
@@ -23,14 +25,19 @@ const IntegrationsSection = () => {
 
   const displayRows = useMemo(() => buildMemberDisplayRows(rows), [rows]);
 
+  const showProgressPanel = MOCK_CONNECTOR_PROGRESS.length > 0;
+
   const handleFilterChange = useCallback((type: SyncFilterType) => {
     setFilterType(type);
     setCurrentPage(1);
   }, []);
 
   return (
-    <section className="flex w-250 flex-col gap-10">
-      <StatusCardsSection cards={cards} />
+    <section className="flex w-250 flex-col gap-8">
+      <div className="flex flex-col gap-3">
+        <StatusCardsSection cards={cards} />
+        {showProgressPanel && <EmbeddingProgressPanel progresses={MOCK_CONNECTOR_PROGRESS} />}
+      </div>
       <UsersStatusSection
         total={total}
         displayRows={displayRows}
