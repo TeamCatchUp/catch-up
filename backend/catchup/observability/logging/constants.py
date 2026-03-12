79c85b733b1e2d48cd4bd8c23083cde626994bae
ba2slk <0730bss@gmail.com>
2026-03-13 03:08:34 +0900

    refactor(backend): logging 성능 최적화 및 책임 분리

--- backend/catchup/observability/logging/constants.py
+++ backend/catchup/observability/logging/constants.py
@@ -2,22 +2,6 @@ import logging
 from typing import Final
 
 
-SENSITIVE_KEYS = {
-    "password",
-    "passphrase",
-    "authorization",
-    "cookie",
-    "set-cookie",
-    "access_token",
-    "refresh_token",
-    "id_token",
-    "token",
-    "client_secret",
-    "api_key",
-    "secret",
-    "private_key",
-}
-
 LOG_LEVELS: Final[dict[str, int]] = {
     "CRITICAL": logging.CRITICAL,
     "ERROR": logging.ERROR,
@@ -25,16 +9,4 @@ LOG_LEVELS: Final[dict[str, int]] = {
     "INFO": logging.INFO,
     "DEBUG": logging.DEBUG,
     "NOTSET": logging.NOTSET,
-}
-
-CONSOLE_EXCLUDE_KEYS: Final[set[str]] = {
-    #"actor",
-    "environment",
-    #"event_action",
-    #"event_type",
-    #"metadata",
-    "service",
-    "trace_id",
-    "version",
-    "remote_addr",
 }
