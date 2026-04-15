47bceb773275c0db5f395cd0a0bd8cfcbf259a40
ba2slk <0730bss@gmail.com>
2026-04-15 14:24:16 +0900

    chore(backend): alembic env.py에 Vector 타입 추가

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -6,6 +6,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+from pgvector.sqlalchemy import Vector
 
 # 프로젝트 경로 설정
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
