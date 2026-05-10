ebb237f13c311f860f60de4308ededb4a9d12fa0
fkgrkyr <fkgrkyr@gmail.com>
2026-05-11 00:46:12 +0900

    fix(frontend): input focus 시 border 1.5px로 변경

--- frontend/src/shared/components/ui/input.tsx
+++ frontend/src/shared/components/ui/input.tsx
@@ -16,8 +16,8 @@ function Input({ className, type, inputSize = 'lg', error = false, ...props }: I
       type={type}
       className={cn(
         'text-body-small text-content-normal placeholder:text-content-assistive bg-fill-normal flex w-full rounded-lg transition-colors focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
-        inputSize === 'lg' && 'border-edge-neutral focus:border-edge-primary gap-3 border p-3',
-        inputSize === 'sm' && 'border-edge-strong focus:border-edge-primary gap-1 border px-2.5 py-1.5',
+        inputSize === 'lg' && 'border-edge-neutral focus:border-edge-primary focus:border-[1.5px] gap-3 border p-3',
+        inputSize === 'sm' && 'border-edge-strong focus:border-edge-primary focus:border-[1.5px] gap-1 border px-2.5 py-1.5',
         error && 'border-status-destructive focus:border-status-destructive',
         className,
       )}
