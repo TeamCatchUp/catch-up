d9b408cb42ce693c2884d6538a1febd90c19990e
fkgrkyr <fkgrkyr@gmail.com>
2026-08-03 19:48:25 +0900

    refactor(frontend): 구 협업툴 연동 레이아웃 폐기
    IntegrationManagementSection은 ConnectorConnectView가, ChannelTalkManagementPanel은 ChannelTalkFlowPanel이 대체했다. /admin/integrations 리다이렉트는 계획 ①에서 이미 있었다.

--- frontend/src/features/admin/integrations/index.ts
+++ frontend/src/features/admin/integrations/index.ts
@@ -1,3 +1,2 @@
 export { default as ConnectorsPageClient } from './components/page/ConnectorsPageClient';
-export { default as IntegrationsPageClient } from './components/page/IntegrationsPageClient';
 export { default as UserMappingPageClient } from './components/page/UserMappingPageClient';
